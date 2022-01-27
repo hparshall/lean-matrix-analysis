1640b234eef20795658bc919ecf8f4c9cfa63a28
Hans Parshall <hparshall@users.noreply.github.com>
2022-01-26 19:05:16 -0800

    progress on 7_35_b_c

--- src/ladr_7_35.lean
+++ src/ladr_7_35.lean
@@ -3,6 +3,7 @@ import analysis.inner_product_space.basic
 import analysis.inner_product_space.pi_L2
 import analysis.inner_product_space.spectrum
 import analysis.normed_space.pi_Lp
+import linear_algebra.basis
 import .lemmas.ladr_7_lem
 import .ladr_7
 
@@ -11,6 +12,21 @@ variables {n : ℕ}
 open_locale big_operators complex_conjugate matrix
 
 notation `is_sa` := inner_product_space.is_self_adjoint
+
+lemma hn : finite_dimensional.finrank ℂ (ℂ^n) = n := by simp
+
+noncomputable def e_vals (T : Lℂ^n) (hsa : is_sa T):=
+  inner_product_space.is_self_adjoint.eigenvalues hsa hn
+
+noncomputable def e_vecs (T : Lℂ^n) (hsa : is_sa T) :=
+  @inner_product_space.is_self_adjoint.eigenvector_basis ℂ _ _ (ℂ^n) _ T hsa _ n hn
+
+noncomputable def scaled_e_vecs (T : Lℂ^n) (hsa : is_sa T) (b : (basis (fin n) ℂ ℂ^n)):
+  (fin n) → ℂ^n :=
+  λ (i : (fin n)), real.sqrt(((e_vals T hsa) i)) • (b i)
+
+noncomputable def sqrt (T : Lℂ^n) (hsa : is_sa T) : Lℂ^n :=
+  @basis.constr (fin n) ℂ (ℂ^n) (ℂ^n) _ _ _ _ _ (e_vecs T hsa) ℂ _ _ _ (scaled_e_vecs T hsa (e_vecs T hsa))
 
 def is_positive (T : Lℂ^n) :=
   (∀ x : ℂ^n, (⟪T x, x⟫_ℂ.re ≥ 0) ∧ (⟪T x, x⟫_ℂ.im = 0))
@@ -62,6 +78,36 @@ theorem thm_7_35_b_c (T : Lℂ^n) (hsa : is_sa T):
   (∀ (μ : ℂ), (T.has_eigenvalue μ) → (μ.re ≥ 0 ∧ μ.im = 0))
     → ∃ (R : Lℂ^n), (R^2 = T) ∧ (is_sa R) ∧ (is_positive R) :=
 begin
+  intro h,
+  use (sqrt T hsa),
+  split,
+  apply basis.ext (e_vecs T hsa),
+  intro i,
+  simp,
+  let vi := (e_vecs T hsa) i,
+  let μi := (e_vals T hsa) i,
+  have fact₁ : T vi = μi • vi :=
+  begin
+    sorry,
+  end,
+  have fact₂ : (sqrt T hsa) vi = (real.sqrt μi) • vi :=
+  begin
+    sorry,
+  end,
+  rw fact₁,
+  rw fact₂,
+  have fact₃ : (sqrt T hsa) (real.sqrt μi • vi) = (real.sqrt μi) • ((sqrt T hsa) vi) :=
+  begin
+    simp,
+  end,
+  rw fact₃,
+  rw fact₂,
+  rw smul_smul,
+  have fact₄ : real.sqrt μi * real.sqrt μi = μi :=
+  begin
+    sorry,
+  end,
+  rw fact₄,
   sorry,
 end
 
@@ -110,8 +156,13 @@ begin
       rw linear_map.adjoint_inner_left,
     end,
     rw fact₁,
-    rw inner_self_eq_norm_sq,
-    simp,
+    norm_cast,
+    have fact₂ : ⟪T v,T v⟫_ℂ.re = is_R_or_C.re ⟪T v,T v⟫_ℂ :=
+    begin
+      simp,
+    end,
+    rw fact₂,
+    apply inner_self_nonneg,
   },
   rw ← complex.eq_conj_iff_im,
   revert v,
