c4ad46267f020b20f5cf83e24f716dd39443665a
Hans Parshall <hparshall@users.noreply.github.com>
2022-01-27 21:51:17 -0800

    7_35 cleanup

--- src/ladr_7_35.lean
+++ src/ladr_7_35.lean
@@ -15,23 +15,44 @@ notation `is_sa` := inner_product_space.is_self_adjoint
 
 lemma hn : finite_dimensional.finrank ℂ (ℂ^n) = n := by simp
 
-noncomputable def e_vals (T : Lℂ^n) (hsa : is_sa T):=
+variables (T : Lℂ^n) (hsa : is_sa T)
+
+noncomputable def e_vals : (fin n → ℝ) :=
   inner_product_space.is_self_adjoint.eigenvalues hsa hn
 
-noncomputable def e_vecs (T : Lℂ^n) (hsa : is_sa T) :=
+noncomputable def e_vecs : (basis (fin n) ℂ (ℂ^n)) :=
   @inner_product_space.is_self_adjoint.eigenvector_basis ℂ _ _ (ℂ^n) _ T hsa _ n hn
 
-noncomputable def scaled_e_vecs (T : Lℂ^n) (hsa : is_sa T) (b : (basis (fin n) ℂ ℂ^n)):
-  (fin n) → ℂ^n :=
-  λ (i : (fin n)), real.sqrt(((e_vals T hsa) i)) • (b i)
+noncomputable def scaled_e_vecs : (fin n) → ℂ^n :=
+  λ (i : (fin n)), real.sqrt(((e_vals T hsa) i)) • ((e_vecs T hsa) i)
 
 noncomputable def sqrt (T : Lℂ^n) (hsa : is_sa T) : Lℂ^n :=
-  @basis.constr (fin n) ℂ (ℂ^n) (ℂ^n) _ _ _ _ _ (e_vecs T hsa) ℂ _ _ _ (scaled_e_vecs T hsa (e_vecs T hsa))
+  @basis.constr (fin n) ℂ (ℂ^n) (ℂ^n) _ _ _ _ _ (e_vecs T hsa) ℂ _ _ _ (scaled_e_vecs T hsa)
 
 def is_positive (T : Lℂ^n) :=
   (∀ x : ℂ^n, (⟪T x, x⟫_ℂ.re ≥ 0) ∧ (⟪T x, x⟫_ℂ.im = 0))
 
-theorem thm_7_35_a_b (T : Lℂ^n) (hsa : is_sa T):
+lemma lem_gram_self_adjoint :
+  is_sa (T.adjoint * T) :=
+begin
+  rw self_adjoint_iff,
+  rw linear_map.eq_adjoint_iff,
+  intros x y,
+  have fact₁ : ⟪((T.adjoint * T) x), y⟫_ℂ = ⟪T x, T y⟫_ℂ :=
+  begin
+    rw ← comp_eq_mul,
+    rw linear_map.adjoint_inner_left,
+  end,
+  have fact₂ : ⟪((T.adjoint * T) x), y⟫_ℂ = ⟪x, (T.adjoint * T) y⟫_ℂ :=
+  begin
+    rw fact₁,
+    rw ← linear_map.adjoint_inner_right,
+    rw comp_eq_mul,
+  end,
+  exact fact₂,
+end
+
+theorem thm_7_35_a_b :
   is_positive T → (∀ (μ : ℂ), (T.has_eigenvalue μ) → (μ.re ≥ 0 ∧ μ.im = 0)) :=
 begin
   intro hpos,
@@ -74,90 +95,68 @@ begin
   exact muim,
 end
 
-theorem thm_7_35_b_c (T : Lℂ^n) (hsa : is_sa T):
-  (∀ (μ : ℂ), (T.has_eigenvalue μ) → (μ.re ≥ 0 ∧ μ.im = 0))
-    → ∃ (R : Lℂ^n), (R^2 = T) ∧ (is_sa R) ∧ (is_positive R) :=
+section sec_7_35_b_c
+
+variable (i : fin n)
+
+variable (hnn : ((e_vals T hsa) i) ≥ 0)
+
+lemma lem_bc_0 :
+  (sqrt T hsa) ((e_vecs T hsa) i) = real.sqrt ((e_vals T hsa) i) • ((e_vecs T hsa) i) :=
 begin
-  intro h,
-  use (sqrt T hsa),
-  split,
+  rw sqrt,
+  rw basis.constr_basis,
+  rw scaled_e_vecs,
+end
+
+lemma lem_bc_1 :
+  ((sqrt T hsa) * (sqrt T hsa)) = T :=
+begin
   apply basis.ext (e_vecs T hsa),
   intro i,
-  simp,
-  let vi := (e_vecs T hsa) i,
   let μi := (e_vals T hsa) i,
-  have fact₁ : T vi = μi • vi :=
-  begin
-    sorry,
-  end,
-  have fact₂ : (sqrt T hsa) vi = (real.sqrt μi) • vi :=
-  begin
-    sorry,
-  end,
-  rw fact₁,
-  rw fact₂,
-  have fact₃ : (sqrt T hsa) (real.sqrt μi • vi) = (real.sqrt μi) • ((sqrt T hsa) vi) :=
-  begin
-    simp,
-  end,
-  rw fact₃,
-  rw fact₂,
-  rw smul_smul,
-  have fact₄ : real.sqrt μi * real.sqrt μi = μi :=
-  begin
-    sorry,
-  end,
-  rw fact₄,
+  let vi := (e_vecs T hsa) i,
+  have h : T vi = μi • vi := by apply inner_product_space.is_self_adjoint.apply_eigenvector_basis,
+  rw h,
+  simp,
+  rw lem_bc_0,
   sorry,
 end
 
-lemma lem_gram_self_adjoint (T : Lℂ^n) :
-  is_sa (T.adjoint * T) :=
+lemma lem_bc_2 :
+  is_sa (sqrt T hsa) :=
 begin
   rw self_adjoint_iff,
-  rw linear_map.eq_adjoint_iff,
-  intros x y,
-  have fact₁ : ⟪((T.adjoint * T) x), y⟫_ℂ = ⟪T x, T y⟫_ℂ :=
-  begin
-    rw ← comp_eq_mul,
-    rw linear_map.adjoint_inner_left,
-  end,
-  have fact₂ : ⟪((T.adjoint * T) x), y⟫_ℂ = ⟪x, (T.adjoint * T) y⟫_ℂ :=
-  begin
-    rw fact₁,
-    rw ← linear_map.adjoint_inner_right,
-    rw comp_eq_mul,
-  end,
-  exact fact₂,
+  rw linear_map.eq_adjoint_iff_basis (e_vecs T hsa) (e_vecs T hsa),
+  intros i₁ i₂,
+  rw lem_bc_0,
+  rw lem_bc_0,
+  sorry,
 end
 
-lemma lem_sqrt_gram (T : Lℂ^n) :
+lemma lem_bc_3 :
+  is_positive (sqrt T hsa) :=
+begin
+  rw is_positive,
+  sorry,
+end
+
+theorem thm_7_35_b_c (hsa : is_sa T) :
+  ∃ (R : Lℂ^n), ((R * R) = T) ∧ (is_sa R) ∧ (is_positive R) :=
+begin
+  use (sqrt T hsa),
+  split,
+  apply lem_bc_1,
+  split,
+  apply lem_bc_2,
+  apply lem_bc_3,
+end
+
+end sec_7_35_b_c
+
+lemma lem_sqrt_gram :
   ∃ (R : Lℂ^n), (R^2 = T.adjoint * T) ∧ (is_sa R) ∧ (is_positive R) :=
 begin
   apply thm_7_35_b_c,
   exact lem_gram_self_adjoint T,
-  apply thm_7_35_a_b,
-  exact lem_gram_self_adjoint T,
-  rw is_positive,
-  intro v,
-  split,
-  {
-    have fact₁ : ⟪(T.adjoint * T) v, v⟫_ℂ = ⟪T v, T v⟫_ℂ :=
-    begin
-      rw ← comp_eq_mul,
-      rw linear_map.adjoint_inner_left,
-    end,
-    rw fact₁,
-    norm_cast,
-    have fact₂ : ⟪T v,T v⟫_ℂ.re = is_R_or_C.re ⟪T v,T v⟫_ℂ :=
-    begin
-      simp,
-    end,
-    rw fact₂,
-    apply inner_self_nonneg,
-  },
-  rw ← complex.eq_conj_iff_im,
-  revert v,
-  rw ← lem_7_15,
-  apply lem_gram_self_adjoint,
 end
