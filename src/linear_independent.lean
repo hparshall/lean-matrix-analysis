a38319df7c558c16b916c6c6471f6841784ead9f
Daniel-Packer <62404584+Daniel-Packer@users.noreply.github.com>
2022-02-04 14:25:44 -0500

    lin_independent proof 2

--- src/linear_independent.lean
+++ src/linear_independent.lean
@@ -84,3 +84,105 @@ begin
   },
   tauto,
 end
+
+
+lemma inside_eq₁ (i j : ι) : finset.filter(λ a : ι, ((a = i) ∨ (a = j)) ∧ (a = i)) finset.univ = finset.filter(λ a : ι, a = i) finset.univ :=
+begin
+  ext,
+  split,
+  intro h,
+  simp at h,
+  simp,
+  exact h.2,
+  intro h,
+  simp at h,
+  simp,
+  split,
+  left,
+  exact h,
+  exact h,
+end
+
+lemma inside_eq₂ (i j : ι) (H : ¬(i = j)) : finset.filter(λ a : ι, ((a = i) ∨ (a = j)) ∧ (¬a = i)) finset.univ = finset.filter(λ a : ι, a = j) finset.univ :=
+begin
+  ext,
+  split,
+  intro h,
+  simp at h,
+  simp,
+  rw or_and_distrib_right at h,
+  simp at h,
+  exact h.1,
+  intro h,
+  simp at h,
+  simp,
+  split,
+  right,
+  exact h,
+  rw ← h at H,
+  intro fals,
+  rw fals at H,
+  exact H rfl,
+end
+
+lemma linear_independent_distinct' (hli : (linear_independent R v)) : v i = v j ↔ i = j :=
+begin
+  rw fintype.linear_independent_iff at hli,
+  split,
+  intro hij,
+  by_contradiction,
+  let g := λ x, ite (x = i ∨ x = j) (ite (x = i) (1 : R) (-1 : R)) (0 : R),
+  specialize hli g,
+  simp at hli,
+  apply hli,
+  simp only [g],
+  calc ∑ (x : ι), ite (x = i ∨ x = j) (ite (x = i) (1 : R) (-1 : R)) 0 • v x = ∑ (x : ι), ite (x = i ∨ x = j) (ite (x = i) (v x) (-(v x))) (0 : M): by {
+    conv
+    {
+      to_lhs,
+      congr,
+      skip,
+      funext,
+      rw ite_smul,
+      rw ite_smul,
+    },
+    simp,
+  }
+    ...                = ∑ (x : ι) in finset.filter (λ x, x = i ∨ x = j) finset.univ, ite (x = i) (v x) (-(v x)) : by {rw finset.sum_ite, rw finset.sum_const_zero, simp}
+    ...                = v i - (v j) : by {rw finset.sum_ite, rw finset.filter_filter, rw finset.filter_filter, rw inside_eq₁, rw inside_eq₂ _ _ h, rw finset.sum_eq_single_of_mem i, rw finset.sum_eq_single_of_mem j, rw sub_eq_add_neg, simp,
+      intros b h₁ h₂,
+      simp at h₁,
+      contradiction,
+      simp,
+      intros b h₁ h₂,
+      simp at h₁,
+      contradiction
+    }
+    ...                = 0 : by {rw hij, rw sub_self},
+  intro h,
+  rw h,
+end
+
+
+-- lemma two_elem_filter (i j : ι) : finset.filter (λ x : ι, x = i ∨ x = j) finset.univ = {i, j} :=
+-- begin
+--   ext,
+--   split,
+--   intro h,
+--   have : (a = i) ∨ (a = j) :=
+--   begin
+--     rw finset.mem_filter at h,
+--     exact h.2,
+--   end,
+--   cases this,
+--   rw this,
+--   simp only [true_or, eq_self_iff_true, finset.mem_insert, finset.mem_singleton],
+--   rw this,
+--   simp only [eq_self_iff_true, or_true, finset.mem_insert, finset.mem_singleton],
+--   intro h,
+--   rw finset.mem_filter,
+--   split,
+--   exact finset.mem_univ a,
+--   sorry,
+  
+-- end
