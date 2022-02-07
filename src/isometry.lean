236194fb64637716ba009f2696cff086e60e5d02
Hans Parshall <hparshall@gmail.com>
2022-02-06 19:43:09 -0800

    progress? on isometry

--- src/isometry.lean
+++ src/isometry.lean
@@ -109,7 +109,9 @@ begin
   exact h,
 end
 
-lemma extend_b_in_Cn : ∃ (u : set ℂ^n) (H : u ⊇ b_in_Cn S) (b : basis u ℂ ℂ^n), orthonormal ℂ b ∧ ⇑b = coe :=
+noncomputable def f := S.subtype ∘ (onbasis S)
+
+lemma extend_b_in_Cn : ∃ (u : set ℂ^n) (H : u ⊇ set.range (f S)) (b : basis u ℂ ℂ^n), orthonormal ℂ b ∧ ⇑b = coe :=
   exists_subset_is_orthonormal_basis (hb_still_on S)
 
 /-
@@ -180,10 +182,10 @@ begin
   rw hr,
 end
 
-lemma extend_Lb_in_Cn : ∃ (u : set ℂ^n) (H : u ⊇ set.range (L ∘ onbasis S)) (b : basis u ℂ ℂ^n), orthonormal ℂ b ∧ ⇑b = coe :=
-  exists_subset_is_orthonormal_basis (hLb_still_on S L)
+noncomputable def g := L ∘ (onbasis S)
 
-noncomputable def f := S.subtype ∘ (onbasis S)
+lemma extend_Lb_in_Cn : ∃ (u : set ℂ^n) (H : u ⊇ set.range (g S L)) (b : basis u ℂ ℂ^n), orthonormal ℂ b ∧ ⇑b = coe :=
+  exists_subset_is_orthonormal_basis (hLb_still_on S L)
 
 lemma onbasis_injective : function.injective (onbasis S) :=
 begin
@@ -199,7 +201,7 @@ begin
   apply function.injective.comp h_sub_inj (onbasis_injective S),
 end
 
-noncomputable def g := L ∘ (onbasis S)
+
 
 lemma g_injective : function.injective (g S L) :=
 begin
@@ -209,81 +211,103 @@ begin
   exact onbasis_injective S,
 end
 
+noncomputable lemma fintype_v : fintype (set.range (f S)) :=
+begin
+  apply set.fintype_range (f S),
+end
+
+lemma h_card_v : @fintype.card (set.range (f S)) (fintype_v S) = fintype.card (orthonormal_basis_index ℂ S) :=
+begin
+  rw set.card_range_of_injective,
+  exact f_injective S,
+end
+
+noncomputable lemma fintype_Lv : fintype (set.range (g S L)) :=
+begin
+  apply set.fintype_range (g S L),
+end
+
+lemma h_card_Lv : @fintype.card (set.range (g S L)) (fintype_Lv S L) = fintype.card (orthonormal_basis_index ℂ S) :=
+begin
+  rw set.card_range_of_injective,
+  exact g_injective S L,
+end
+
+lemma h_card_v_Lv :  @fintype.card (set.range (f S)) (fintype_v S) = @fintype.card (set.range (g S L)) (fintype_Lv S L) :=
+begin
+  rw h_card_v,
+  rw h_card_Lv,
+end
+
+def u := classical.some (extend_b_in_Cn S)
+
+noncomputable def b_u := (extend_b_in_Cn S).some_spec.some_spec.some
+
+noncomputable lemma fintype_u : fintype (u S) := by apply finite_dimensional.fintype_basis_index (b_u S)
+
+lemma h_v_u : (set.range (f S) ⊆ u S) := (extend_b_in_Cn S).some_spec.some
+
+lemma h_f : ∀ (x : (orthonormal_basis_index ℂ S)), (f S x) ∈ (u S) :=
+begin
+  intro x,
+  apply set.mem_of_subset_of_mem (h_v_u S),
+  apply set.mem_range_self x,
+end
+
+def Mu := classical.some (extend_Lb_in_Cn S L)
+
+noncomputable def b_Mu := (extend_Lb_in_Cn S L).some_spec.some_spec.some
+
+noncomputable lemma fintype_Mu : fintype (Mu S L) := by apply finite_dimensional.fintype_basis_index (b_Mu S L)
+
+lemma h_card_u_Mu : @fintype.card (u S) (fintype_u S) = @fintype.card (Mu S L) (fintype_Mu S L) :=
+begin
+  rw ← @finite_dimensional.finrank_eq_card_basis _ _ _ _ _ _ (fintype_u S) (b_u S),
+  rw ← @finite_dimensional.finrank_eq_card_basis _ _ _ _ _ _ (fintype_Mu S L) (b_Mu S L),
+end
+
+lemma h_Lv_Mu : set.range (g S L) ⊆ (Mu S L) := (extend_Lb_in_Cn S L).some_spec.some
+
+def h_Mu := classical.some_spec (extend_Lb_in_Cn S L)
+
+noncomputable def f_to_u := set.cod_restrict (f S) (u S) (h_f S)
+
 lemma L_to_M : ∃ (M : (ℂ^n) →ₗᵢ[ℂ] (ℂ^n)), (∀ (s : S), M s = L s) :=
 begin
-  let v := set.range (f S),
-  have hb : ∃ (u : set ℂ^n) (H : u ⊇ v) (b : basis u ℂ ℂ^n), orthonormal ℂ b ∧ ⇑b = coe :=
-    by apply extend_b_in_Cn S,
-  cases hb with u hb,
-  cases hb with h_uv hb,
-  cases hb with b_u hb,
-  cases hb with hb_on hb_coe,
-
-  let Lv := set.range (g S L),
-  have hLb : ∃ (Mu : set ℂ^n) (H : Mu ⊇ Lv) (b : basis Mu ℂ ℂ^n), orthonormal ℂ b ∧ ⇑b = coe :=
-    by apply extend_Lb_in_Cn S L,
-  cases hLb with Mu hLb,
-  cases hLb with h_MuLv hLb,
-  cases hLb with b_Mu hLb,
-  cases hLb with hLb_on hLb_coe,
-
-  have h_f : ∀ (x : (orthonormal_basis_index ℂ S)), (f S x) ∈ u :=
-  begin
-    intro x,
-    apply set.mem_of_subset_of_mem h_uv,
-    apply set.mem_range_self,
-  end,
-
-  let f_to_u := set.cod_restrict (f S) u h_f,
-
-  have fintype_u : fintype u := by apply finite_dimensional.fintype_basis_index b_u,
-  have fintype_Mu : fintype Mu := by apply finite_dimensional.fintype_basis_index b_Mu,
   
-  have : @fintype.card u fintype_u = @fintype.card Mu fintype_Mu :=
-  begin
-    rw ← @finite_dimensional.finrank_eq_card_basis _ _ _ _ _ _ fintype_u b_u,
-    rw ← @finite_dimensional.finrank_eq_card_basis _ _ _ _ _ _ fintype_Mu b_Mu,
-  end,
-
-  have fintype_v : fintype v := set.fintype_range (f S),
-  have fintype_Lv : fintype Lv := set.fintype_range (g S L),
-
-  have h_cardv_S : @fintype.card v fintype_v = fintype.card (orthonormal_basis_index ℂ S) :=
-  begin
-    rw set.card_range_of_injective,
-    exact f_injective S,
-  end,
-
-  have h_cardLv_S : @fintype.card Lv fintype_Lv = fintype.card (orthonormal_basis_index ℂ S) :=
-  begin
-    rw set.card_range_of_injective,
-    exact g_injective S L,
-  end,
-
-  have : @fintype.card v fintype_v = @fintype.card Lv fintype_Lv :=
-  begin
-    rw h_cardv_S,
-    rw h_cardLv_S,
-  end,
-
-  let vc := u.diff v,
-  let Lvc := Mu.diff Lv,
+  let vc := (u S) \ set.range (f S),
+  let Lvc := (Mu S L) \ set.range (g S L),
 
   have fintype_vc : fintype vc := 
   begin
-    apply @set.fintype_subset _ u _ fintype_u _ _,
+    apply @set.fintype_subset _ (u S) _ (fintype_u S) _ _,
     exact classical.dec_pred (λ (_x : ℂ^n), _x ∈ vc),
     apply set.diff_subset,
   end,
 
   have fintype_Lvc : fintype Lvc := 
   begin
-    apply @set.fintype_subset _ Mu _ fintype_Mu _ _,
+    apply @set.fintype_subset _ (Mu S L) _ (fintype_Mu S L) _ _,
     exact classical.dec_pred (λ (_x : ℂ^n), _x ∈ Lvc),
     apply set.diff_subset,
   end,
 
-  have h_card_vc : @fintype.card vc fintype_vc = @fintype.card Lvc fintype_Lvc := sorry,
+  have h_card_vc : @fintype.card vc fintype_vc = @fintype.card Lvc fintype_Lvc :=
+  begin
+    rw ← set.to_finset_card,
+    rw ← set.to_finset_card,
+    rw @set.to_finset_sdiff _ _ _ _ (fintype_u S) (fintype_v S) fintype_vc,
+    rw @set.to_finset_sdiff _ _ _ _ (fintype_Mu S L) (fintype_Lv S L) fintype_Lvc,
+    rw finset.card_sdiff,
+    rw finset.card_sdiff,
+    simp only [set.to_finset_card],
+    rw h_card_v_Lv,
+    rw h_card_u_Mu,
+    rw set.to_finset_mono,
+    exact h_Lv_Mu S L,
+    rw set.to_finset_mono,
+    exact h_v_u S,
+  end,
 
   have h_e1 : ∃ (e1 : vc ≃ Lvc), true :=
   begin
@@ -293,6 +317,10 @@ begin
   end,
 
   let e1 := classical.some h_e1,
+
+  clear h_card_vc,
+  clear fintype_vc,
+  clear fintype_Lvc,
 
   let e2 := ⇑e1,
 
@@ -310,14 +338,16 @@ begin
     exact equiv.injective e1,
   end,
   
-  have h_vc : vc ⊆ u := set.diff_subset u v,
+  have h_vc : vc ⊆ (u S) := set.diff_subset (u S) _,
 
-  have h_e' : ∃ (e' : u → ℂ^n), ∀ (x : vc), e' ((set.inclusion h_vc x)) = e x :=
+  let e' := function.extend (set.inclusion h_vc) e (0 : (u S) → (ℂ^n)),
+
+  have h_e' : ∀ (x : vc), e' ((set.inclusion h_vc x)) = e x :=
   begin
-    sorry,
+    intro x,
+    apply function.extend_apply,
+    exact set.inclusion_injective h_vc,
   end,
-
-  let e' := classical.some h_e',
 
   /-
   function.extend gives a map M : u → ℂ^n where for x = f i ∈ v,
@@ -327,13 +357,59 @@ begin
   e is an arbitrary bijection between u ∖ v and Mu ∖ Lv
   -/
 
-  let M1 := function.extend f_to_u (g S L) e',
+  let M1 := function.extend (f_to_u S) (g S L) e',
 
-  let M2 := (basis.constr b_u ℂ) M1,
+  let M2 := (basis.constr (b_u S) ℂ) M1,
 
-  have h_Mb_on : orthonormal ℂ (⇑M2 ∘ ⇑b_u) := sorry,
+  have h_f_to_u_injective : function.injective (f_to_u S) := sorry,
 
-  let M := M2.isometry_of_orthonormal hb_on h_Mb_on,
+  have h_Mb_on : orthonormal ℂ (⇑M2 ∘ ⇑(b_u S)) :=
+  begin
+    rw orthonormal_iff_ite,
+    intros i j,
+    rw function.comp_app,
+    rw function.comp_app,
+    simp only [basis.constr_basis],
+    by_cases hx: (∃ (x : (orthonormal_basis_index ℂ S)), (f_to_u S x) = i),
+    by_cases hy: (∃ (y : (orthonormal_basis_index ℂ S)), (f_to_u S y) = j),
+    let h_x_to_i := classical.some_spec hx,
+    let h_y_to_j := classical.some_spec hy,
+    rw ← h_x_to_i,
+    rw ← h_y_to_j,
+    simp only [M1],
+    rw function.extend_apply,
+    rw function.extend_apply,
+    have : orthonormal ℂ (g S L) := sorry,
+    rw orthonormal_iff_ite at this,
+    specialize this (classical.some hx) (classical.some hy),
+    rw this,
+    by_cases h_xy : (classical.some hx) = (classical.some hy),
+    rw h_xy,
+    simp only [if_true, eq_self_iff_true],
+    have : ite (classical.some hx = classical.some hy) (1 : ℂ) (0 : ℂ) = (0 : ℂ) :=
+    begin
+      rw ne.ite_eq_right_iff,
+      exact h_xy,
+      simp only [ne.def, not_false_iff, one_ne_zero],
+    end,
+    rw this,
+    symmetry,
+    rw ne.ite_eq_right_iff,
+    apply function.injective.ne h_f_to_u_injective,
+    exact h_xy,
+    simp only [ne.def, not_false_iff, one_ne_zero],
+    exact h_f_to_u_injective,
+    exact h_f_to_u_injective,
+    simp only [M1],
+    rw function.extend_apply' _ _ _ hy,
+    let h_x_to_i := classical.some_spec hx,
+    rw ← h_x_to_i,
+    rw function.extend_apply h_f_to_u_injective,
+    sorry,
+    sorry,
+  end,
+
+  let M := M2.isometry_of_orthonormal (hb_on S) h_Mb_on,
   use M,
   intro s,
   simp only [M],
