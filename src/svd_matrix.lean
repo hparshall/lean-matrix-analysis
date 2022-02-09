dc2a82226328c021301375028e1efe94c77c14ba
Daniel-Packer <62404584+Daniel-Packer@users.noreply.github.com>
2022-02-09 09:38:13 -0500

    working towards matrix svd

--- src/svd_matrix.lean
+++ src/svd_matrix.lean
@@ -1,9 +1,25 @@
 import .svd
 import data.matrix.basic
 
+
+
+-- -- theorem svd_matrix : ∃ U Σ V : matrix (fin n) (fin n) ℂ, A = U * Σ * V ∧ (Uᴴ = U ∧ Vᴴ = V) :=
+-- -- sorry
+
 variables {n : ℕ} (A : matrix (fin n) (fin n) ℂ)
 
--- theorem svd_matrix : ∃ U Σ V : matrix (fin n) (fin n) ℂ, A = U * Σ * V ∧ (Uᴴ = U ∧ Vᴴ = V) :=
--- sorry
+open_locale matrix
 
-example : ∃ U : matrix (fin n) (fin n) ℂ, A = U := sorry
+def matrix_to_lin_end (B : matrix (fin n) (fin n) ℂ) : (ℂ^n) → (ℂ^n) :=
+begin
+  let T := (id (B.to_lin') : (ℂ^n) → ℂ^n),
+  exact T,
+end
+
+
+example : ∃ (U V: matrix (fin n) (fin n) ℂ) (s : (fin n) → ℂ), A = U * (matrix.diagonal s) * Vᴴ ∧ (Uᴴ = U) ∧ (Vᴴ = V) :=
+begin
+  let T := (id(A.to_lin') : Lℂ^n),
+  have svd_T := svd T,
+  
+end
