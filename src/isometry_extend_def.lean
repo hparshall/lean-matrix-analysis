e8bfcb362a37dd2cbd10b6cc7c7d9a37a64aad64
Hans Parshall <hparshall@gmail.com>
2022-02-19 09:58:09 -0800

    isolating gram lemmas, cleaning up isometry extend

--- src/isometry_extend_def.lean
+++ src/isometry_extend_def.lean
@@ -1,58 +1,42 @@
-/-
-The goal of this file is to prove the following.  If S ⊆ ℂ^n is a subspace and L : S → V is
-an isometry, then there exists an isometry M : ℂ^n → ℂ^n such that M(s) = L(s) for every s ∈ S.
-
--- Checked over by: 
--- Hans
--/
-
+import analysis.inner_product_space.projection
 import analysis.inner_product_space.pi_L2
-open_locale complex_conjugate
 
-variable {n : ℕ}
+variables {E : Type*}
+[inner_product_space ℂ E]
+[finite_dimensional ℂ E]
 
-local notation `ℂ^` n := euclidean_space ℂ (fin n)
-
-variables {S : submodule ℂ (ℂ^n)} {L : S →ₗᵢ[ℂ] ℂ^n}
+variables {S : submodule ℂ E} {L : S →ₗᵢ[ℂ] E}
 
 open finite_dimensional
-/-
-dim L(S) = dim S
--/
+
 lemma finrank_range_of_inj (hL : function.injective L) :
   (finrank ℂ L.to_linear_map.range) = (finrank ℂ S) :=
 begin
   sorry,
 end
 
-
-/-
-dim L(S)ᗮ = dim Sᗮ
--/
 lemma finrank_orthogonal_range_of_inj (hL: function.injective L) :
   (finrank ℂ (L.to_linear_map.range)ᗮ) = (finrank ℂ Sᗮ) :=
 begin
-  have : (finrank ℂ (L.to_linear_map.range)ᗮ) = (finrank ℂ ℂ^n) - (finrank ℂ (L.to_linear_map.range)) :=
+  have : (finrank ℂ (L.to_linear_map.range)ᗮ) = (finrank ℂ E) - (finrank ℂ (L.to_linear_map.range)) :=
     by simp only [← (L.to_linear_map.range).finrank_add_finrank_orthogonal, add_tsub_cancel_left, eq_self_iff_true],
   rw this,
-  have : (finrank ℂ Sᗮ) = (finrank ℂ ℂ^n) - (finrank ℂ S) :=
+  have : (finrank ℂ Sᗮ) = (finrank ℂ E) - (finrank ℂ S) :=
     by simp only [← S.finrank_add_finrank_orthogonal, add_tsub_cancel_left, eq_self_iff_true],
   rw this,
   have : (finrank ℂ (L.to_linear_map.range)) = (finrank ℂ S) :=
-  begin
-    rw finrank_range_of_inj hL,
-  end,
+    finrank_range_of_inj hL,
   rw this,
 end
 
-lemma norm_sq_eq_norm_sq_proj_orthogonal (x : ℂ^n) (S : submodule ℂ ℂ^n) :
-  ∥ x ∥^2 = ∥ (orthogonal_projection S).to_linear_map x ∥^2 + ∥ (orthogonal_projection Sᗮ).to_linear_map x ∥^2 :=
+lemma norm_sq_eq_norm_sq_projection (x : E) (S : submodule ℂ E) :
+  ∥ x ∥^2 = ∥ (orthogonal_projection S) x ∥^2 + ∥ (orthogonal_projection Sᗮ) x ∥^2 :=
 begin
-  let p1 := (orthogonal_projection S).to_linear_map,
-  let p2 := (orthogonal_projection Sᗮ).to_linear_map,
-  have x_decomp : x = (p1 x : ℂ^n) + (p2 x) :=  
+  let p1 := (orthogonal_projection S),
+  let p2 := (orthogonal_projection Sᗮ),
+  have x_decomp : x = (p1 x : E) + (p2 x) :=  
     eq_sum_orthogonal_projection_self_orthogonal_complement S x,    
-  have x_orth : ⟪ (p1 x : ℂ^n), p2 x ⟫_ℂ = 0 :=
+  have x_orth : ⟪ (p1 x : E), p2 x ⟫_ℂ = 0 :=
   begin
     have p1x : ↑(p1 x) ∈ S := set_like.coe_mem (p1 x),
     have p2x : ↑(p2 x) ∈ Sᗮ := set_like.coe_mem (p2 x),
@@ -64,66 +48,96 @@ begin
     rw x_decomp,
   end,
   simp only [sq],
-  rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero ((p1 x) : ℂ^n) (p2 x) x_orth,
-  simp only [add_left_inj,
-    mul_eq_mul_left_iff,
-    norm_eq_zero,
-    true_or,
-    eq_self_iff_true,
-    submodule.coe_norm,
-    submodule.coe_eq_zero],
+  rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero ((p1 x) : E) (p2 x) x_orth,
+  simp only [add_left_inj, mul_eq_mul_left_iff, norm_eq_zero, true_or, eq_self_iff_true,
+    submodule.coe_norm, submodule.coe_eq_zero],
 end
 
-noncomputable def linear_isometry_extend (L : S →ₗᵢ[ℂ] ℂ^n): (ℂ^n) →ₗᵢ[ℂ] (ℂ^n) :=
-begin
-  let p1 := (orthogonal_projection S).to_linear_map,
-  let p2 := (orthogonal_projection Sᗮ).to_linear_map,
-  have : (finrank ℂ Sᗮ) = (finrank ℂ Sᗮ) := rfl,
-  let L2_first := (linear_isometry_equiv.of_inner_product_space this).to_linear_isometry,
-  let L2_next := (linear_isometry_equiv.of_inner_product_space (finrank_orthogonal_range_of_inj L.injective)).symm.to_linear_isometry,
-  let L2 := (L.to_linear_map.range)ᗮ.subtypeₗᵢ.comp (L2_next.comp L2_first),
-  let M := L.to_linear_map.comp p1 + L2.to_linear_map.comp p2,
-  have : linear_isometry _ (ℂ^n) (ℂ^n) :=
+namespace linear_isometry
+
+noncomputable def extend (L : S →ₗᵢ[ℂ] E): E →ₗᵢ[ℂ] E :=
   {
-    to_linear_map := M,
-    norm_map' := _,
-  },
-  exact this,
-  intro x,
-
-  have Mx_decomp : M x = L (p1 x) + L2 (p2 x) :=
-  begin
-    simp only [linear_map.add_apply],
-    rw [linear_map.comp_apply, linear_map.comp_apply],
-    simp only [linear_isometry.coe_to_linear_map],
-  end,
-  have Mx_orth : ⟪ L (p1 x), L2 (p2 x) ⟫_ℂ = 0 :=
-  begin
-    have Lp1x : L (p1 x) ∈ L.to_linear_map.range := L.to_linear_map.mem_range_self (p1 x),
-    have Lp2x : L2 (p2 x) ∈ (L.to_linear_map.range)ᗮ :=
+    to_linear_map :=
     begin
-      simp only [L2],
-      simp only [linear_isometry.coe_comp,
-      function.comp_app,
-      submodule.coe_subtypeₗᵢ],
-      have : (L.to_linear_map.range)ᗮ = (L.to_linear_map.range)ᗮ.subtype.range :=
-        by simp only [submodule.range_subtype, eq_self_iff_true],
-      conv
-      begin
-        congr,
-        skip,
-        rw this,
-      end,
-      apply linear_map.mem_range_self,
+      -- Project onto S and Sᗮ
+      let p1 := (orthogonal_projection S).to_linear_map,
+      let p2 := (orthogonal_projection Sᗮ).to_linear_map,
+      -- Build an isometry from Sᗮ to L(S)ᗮ through euclidean_space
+      let L1 := (linear_isometry_equiv.of_inner_product_space
+        (@rfl _ (finrank ℂ Sᗮ))).to_linear_isometry,
+      let L2 := (linear_isometry_equiv.of_inner_product_space
+        (finrank_orthogonal_range_of_inj L.injective)).symm.to_linear_isometry,
+      let L3 := (L.to_linear_map.range)ᗮ.subtypeₗᵢ.comp (L2.comp L1),
+      -- Build a linear map from the isometries on S and Sᗮ
+      let M := L.to_linear_map.comp p1 + L3.to_linear_map.comp p2,
+      exact M,
     end,
-    apply submodule.inner_right_of_mem_orthogonal Lp1x Lp2x,
-  end,
-  rw ← sq_eq_sq,
-  rw norm_sq_eq_norm_sq_proj_orthogonal x S,
-  simp only [sq],
-  rw Mx_decomp,
-  rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero (L (p1 x)) (L2 (p2 x)) Mx_orth,
-  simp only [linear_isometry.norm_map],
-  exact norm_nonneg _,
-  exact norm_nonneg _,
+    norm_map' :=
+    begin
+      simp only [],
+      -- Same notation as above
+      let p1 := (orthogonal_projection S).to_linear_map,
+      let p2 := (orthogonal_projection Sᗮ).to_linear_map,
+      let L1 := (linear_isometry_equiv.of_inner_product_space
+        (@rfl _ (finrank ℂ Sᗮ))).to_linear_isometry,
+      let L2 := (linear_isometry_equiv.of_inner_product_space
+        (finrank_orthogonal_range_of_inj L.injective)).symm.to_linear_isometry,
+      let L3 := (L.to_linear_map.range)ᗮ.subtypeₗᵢ.comp (L2.comp L1),
+      let M := L.to_linear_map.comp p1 + L3.to_linear_map.comp p2,
+      intro x,
+      -- Apply M to the orthogonal decomposition of x
+      have Mx_decomp : M x = L (p1 x) + L3 (p2 x) :=
+      begin
+        simp only [linear_map.add_apply],
+        rw [linear_map.comp_apply, linear_map.comp_apply],
+        simp only [linear_isometry.coe_to_linear_map],
+      end,
+      -- Mx_decomp is the orthogonal decomposition of M x
+      have Mx_orth : ⟪ L (p1 x), L3 (p2 x) ⟫_ℂ = 0 :=
+      begin
+        have Lp1x : L (p1 x) ∈ L.to_linear_map.range := L.to_linear_map.mem_range_self (p1 x),
+        have Lp2x : L3 (p2 x) ∈ (L.to_linear_map.range)ᗮ :=
+        begin
+          simp only [L3, linear_isometry.coe_comp, function.comp_app, submodule.coe_subtypeₗᵢ],
+          have : (L.to_linear_map.range)ᗮ = (L.to_linear_map.range)ᗮ.subtype.range :=
+            by simp only [submodule.range_subtype, eq_self_iff_true],
+          conv
+          begin
+            congr,
+            skip,
+            rw this,
+          end,
+          apply linear_map.mem_range_self,
+        end,
+        apply submodule.inner_right_of_mem_orthogonal Lp1x Lp2x,
+      end,
+      rw ← sq_eq_sq,
+      -- Apply the Pythagorean theorem and simplify
+      rw norm_sq_eq_norm_sq_projection x S,
+      simp only [sq, Mx_decomp],
+      rw norm_add_sq_eq_norm_sq_add_norm_sq_of_inner_eq_zero (L (p1 x)) (L3 (p2 x)) Mx_orth,
+      simp only [linear_isometry.norm_map, p1, p2, continuous_linear_map.to_linear_map_eq_coe,
+        add_left_inj, mul_eq_mul_left_iff, norm_eq_zero, true_or, eq_self_iff_true,
+        continuous_linear_map.coe_coe, submodule.coe_norm, submodule.coe_eq_zero],
+      exact norm_nonneg _,
+      exact norm_nonneg x,
+    end
+  }
+
+lemma extend_extends (L : S →ₗᵢ[ℂ] E) (s : S):
+  extend L s = L s :=
+begin
+  rw extend,
+  push_cast,
+  simp only [continuous_linear_map.to_linear_map_eq_coe],
+  rw ← linear_isometry.coe_to_linear_map,
+  simp only [add_right_eq_self, linear_isometry.coe_to_linear_map, 
+    linear_isometry_equiv.coe_to_linear_isometry, linear_isometry.coe_comp, function.comp_app,
+    orthogonal_projection_mem_subspace_eq_self, linear_map.coe_comp, continuous_linear_map.coe_coe,
+    submodule.coe_subtype, linear_map.add_apply, submodule.coe_eq_zero, 
+    linear_isometry_equiv.map_eq_zero_iff, submodule.coe_subtypeₗᵢ],
+  rw orthogonal_projection_mem_subspace_orthogonal_complement_eq_zero,
+  simp only [submodule.orthogonal_orthogonal, submodule.coe_mem],
 end
+
+end linear_isometry
